e0c7ea188326a50a8e67028259dd232fe2e2ea6d
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2022-11-08 09:15:41 +0100

    test more endpoint

--- src/tests/blog.spec.js
+++ src/tests/blog.spec.js
@@ -33,7 +33,7 @@ describe('Blog route', () => {
     // create blog in our db
     await Blog.create({
       title: 'The fault in our stars',
-      description: 'A top-rated prose series...',
+      description: 'A top-rated TV series...',
       created_at: moment().toDate(),
       body: 'Once upon a time',
       author: [
@@ -47,7 +47,31 @@ describe('Blog route', () => {
     });
 
     const response = await request(app)
-      .get('/orders')
+      .get('/api/blog/all')
+      .set('content-type', 'application/json')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('newBlog');
+    expect(response.body).toHaveProperty('status', true);
+  });
+  it('should return all known blogs', async () => {
+    await Blog.create({
+      title: 'The fault in our stars',
+      description: 'A top-rated TV series...',
+      created_at: moment().toDate(),
+      body: 'Once upon a time',
+      author: [
+        {
+          email: 'janedoe@gmail.com',
+          first_name: 'Jane',
+          last_name: 'Doe',
+          username: 'jannie',
+        },
+      ],
+    });
+    const response = await request(app)
+      .get('/api/blog/all?state=published')
       .set('content-type', 'application/json')
       .set('Authorization', `Bearer ${token}`);
 
